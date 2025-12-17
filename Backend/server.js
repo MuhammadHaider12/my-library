387c6c11694304784aca3580329fc5b0b4861b40
MuhammadHaider12 <haiderarshad660@gmail.com>
2025-12-17 14:10:18 +0500

    ahhhhhhhhhhhhhhhhhhhhhhhhh

--- Backend/server.js
+++ Backend/server.js
@@ -38,7 +38,7 @@ const bcrypt = require('bcrypt');
 
 // --- Middleware ---
 app.use(cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:5501', 'https://my-library-blush.vercel.app/'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
